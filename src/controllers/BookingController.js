fe4c59412b873a0b6d54488ec56ce8ffe528d125
Rafael Scholant <rafael.scholant@gmail.com>
2019-10-04 14:44:47 -0300

    cors and /files added

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -4,7 +4,7 @@ module.exports = {
   async store(req, res) {
     const { user_id } = req.headers;
     const { spot_id } = req.params;
-    const { date } = req.body;
+    const { date } = req.bod;                                                                                                                                                                                                                                                                 y;
     const booking = await Booking.create({
       user: user_id,
       spot: spot_id,
